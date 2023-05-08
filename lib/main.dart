6b1c1d0f91dc7cd68e9c12141f82f192ed976714
Edria Taji <116660805+edris-taji@users.noreply.github.com>
2023-05-08 11:55:08 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,53 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           backgroundColor: Colors.red,
-          title: Text('Dice'),
+          title: const Text('Dice'),
         ),
-        body: MyApp(),
+        body: const Dice(),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class Dice extends StatefulWidget {
+  const Dice({super.key});
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changeDices() {
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            child: Image(image: AssetImage('images/dice$leftDice.png')),
+            onPressed: () {
+              setState(() {
+                changeDices();
+              });
+            },
+          )),
+          Expanded(
+              child: TextButton(
+            child: Image(image: AssetImage('images/dice$rightDice.png')),
+            onPressed: () {
+              setState(() {
+                changeDices();
+              });
+            },
+          )),
+        ],
+      ),
+    );
   }
 }
